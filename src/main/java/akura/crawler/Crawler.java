2deebbf9f51fd132158453eabc8740e87ede65b1
Nipuna <nipuna@app-monkeyz.com>
2017-07-09 15:16:52 +0530

    Add jsoup dependency, crawler implementation, reviews model

---
+++ src/main/java/akura/crawler/Crawler.java
@@ -0,0 +1,118 @@
+package akura.crawler;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Locale;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawler {
+	public static ArrayList<Review> reviewList = new ArrayList<Review>();
+    ArrayList<Integer>  pagesScraped;
+	public void fetchReview(String itemID) {
+        String url = "http://www.amazon.com/product-reviews/" + itemID
+                + "/?showViewpoints=0&sortBy=byRankDescending&pageNumber=" + 0; //(1 + (int) (Math.random() * 10))
+        // http://www.amazon.com/product-reviews/B00UC9QKQ2/?showViewpoints=0&sortBy=byRankDescending&pageNumber=0
+        //Modify this file name to reflect the name of the product you are reviewing.
+        try {
+            // Get the max number of review pages;
+            org.jsoup.nodes.Document reviewpage1 = null;
+            reviewpage1 = Jsoup.connect(url).timeout(10 * 1000).get();
+            int maxpage = 1;
+            Elements pagelinks = reviewpage1.select("a[href*=pageNumber=]");
+            
+            System.out.println("**Page Links**" + pagelinks);
+            
+            if (pagelinks.size() != 0) {
+                ArrayList<Integer> pagenum = new ArrayList<Integer>();
+                for (Element link : pagelinks) {
+                    try {
+                    	System.out.println("**link**" + link);
+                        pagenum.add(Integer.parseInt(link.text()));
+                    } catch (NumberFormatException nfe) {
+                    }
+                }
+                maxpage = Collections.max(pagenum);
+            }
+            // collect review from each of the review pages;
+            int p = 0;
+            while (p <= maxpage) {
+                System.out.println("Now in Page: " + p);
+
+                url = "http://www.amazon.com/product-reviews/"
+                        + itemID
+                        + "/?sortBy=helpful&pageNumber="
+                        + p;
+                org.jsoup.nodes.Document reviewpage = null;
+                reviewpage = Jsoup.connect(url).timeout(10 * 1000).get();
+                if (reviewpage.select("div.a-section.review").isEmpty()) {
+                	System.out.println("Review section is empty");
+                } else {
+                    Elements reviewsHTMLs = reviewpage.select(
+                            "div.a-section.review");
+                    for (Element reviewBlock : reviewsHTMLs) {
+                        Review theReview = parseReview(reviewBlock, url);
+                        reviewList.add(theReview);
+                        theReview.printReviews();
+                    }
+                }
+
+                if(p==1) {
+                	break;
+                }
+                p++;
+            }
+        } catch (Exception e) {
+//        	e.printStackTrace();
+            try {
+                Thread.sleep((int)(1000.0 + Math.random() * 10000));
+                fetchReview(itemID);
+                e.printStackTrace();
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+	
+    public Review parseReview(Element reviewBlock, String url) throws ParseException {
+        String review_id = "";
+        String title = "";
+        int rating = 0;
+        String reviewContent = "";
+        int classLabel = 0;
+
+        // review id
+        review_id = reviewBlock.id();
+
+        // title
+        Element reviewTitle = reviewBlock.select("a.review-title").first();
+        title = reviewTitle.text();
+
+        // rating
+        Element star = reviewBlock.select("i.a-icon-star").first();
+        String starinfo = star.text();
+        rating = Integer.parseInt(starinfo.substring(0, 1));
+
+        //class label
+        classLabel = rating;
+
+        // review date
+        Elements date = reviewBlock.select("span.review-date");
+        String datetext = date.first().text();
+        datetext = datetext.substring(3); // remove "On "
+        Date reviewDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+                .parse(datetext);
+
+        // review content
+        Element contentDoc = reviewBlock.select("span.review-text").first();
+        reviewContent = contentDoc.text();
+
+        return new Review(review_id, title, rating, url, reviewDate, reviewContent, classLabel);
+
+    }
+}
